a4a053cb0c6368a4be7c47e4d0575f4fa7a3027f
rwasabahizi <rwasabahizijustin@gmail.com>
2019-07-22 10:26:53 +0100

    my first commit

---
+++ function.c
@@ -0,0 +1,45 @@
+#include<stdio.h>
+/*int addition(int a ,int b);
+void main()
+{
+    int x,y,sum=0;
+    printf("enter two number for add\n");
+    scanf("%d %d",&x,&y);
+    sum=addition(x,y);
+    printf("sum=%d",sum);
+}
+int addition(int a ,int b)
+{
+int s=a+b;
+
+
+return s;
+}
+int mult(int x ,int y);
+void main()
+{
+    int a,b,c;
+    printf("enter two number\n");
+    scanf("%d %d",&a,&b);
+    c=mult(a,b);
+    printf("\n product is=%d",c);
+
+}int mult(int x ,int y)
+{
+int q=x*y;
+ return q;
+}*/
+int div(int x ,int y );
+void main()
+{
+    int a,b,c;
+    printf("enter any thing\n");
+    scanf("%d %d",&a,&b);
+    c=div(a,b);
+    printf("div%d:\n",c);
+}int div(int x,int y)
+{
+    int z=x/y;
+    rerun z;
+}
+
