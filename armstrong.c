e61aebffe46a33816cfee0f76c612b56bdc7542e
rwasabahizi <rwasabahizijustin@gmail.com>
2019-07-22 10:38:09 +0100

    my first commit

---
+++ armstrong.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+int main()
+{
+    int number, sum=0, rem=0,tempNumber;
+
+    printf("Enter an integer number: ");
+    scanf("%d", &number);
+
+    tempNumber=number;
+
+    while(tempNumber!=0)
+    {
+        rem=tempNumber%10;
+        sum=sum + (rem*rem*rem);
+        tempNumber/=10;
+    }
+
+/* checking number is armstrong or not */
+    if(sum==number)
+        printf("%d is an Armstrong number.",number);
+    else
+        printf("%d is not an Armstrong number.",number);
+
+    return 0;
+}
